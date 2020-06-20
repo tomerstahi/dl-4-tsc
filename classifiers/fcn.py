5f42fab38743347e54fb150111f0f23504a7f4eb
tomerstahi <tomer1231@gmail.com>
2020-06-20 15:14:26 +0300

    Update fcn.py
    temporairly commented out the save_logs function

--- classifiers/fcn.py
+++ classifiers/fcn.py
@@ -83,7 +83,7 @@ class Classifier_FCN:
 		# convert the predicted from binary to integer 
 		y_pred = np.argmax(y_pred , axis=1)
 
-		save_logs(self.output_directory, hist, y_pred, y_true, duration)
+# 		save_logs(self.output_directory, hist, y_pred, y_true, duration)
 
 		keras.backend.clear_session()
 
