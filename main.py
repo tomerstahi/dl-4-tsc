855a6156892b4fd56cb5515e15520e5955703fb3
tomerstahi <tomer1231@gmail.com>
2020-06-22 07:06:57 +0300

    Update main.py
    commented out the y_train and y_test transformation (throws a dimensions mismatch error )

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ def fit_classifier():
     # transform the labels from integers to one hot vectors
     enc = sklearn.preprocessing.OneHotEncoder(categories='auto')
     enc.fit(np.concatenate((y_train, y_test), axis=0).reshape(-1, 1))
-    y_train = enc.transform(y_train.reshape(-1, 1)).toarray()
-    y_test = enc.transform(y_test.reshape(-1, 1)).toarray()
+#     y_train = enc.transform(y_train.reshape(-1, 1)).toarray()
+#     y_test = enc.transform(y_test.reshape(-1, 1)).toarray()
 
     # save orignal y because later we will use binary
     y_true = np.argmax(y_test, axis=1)
